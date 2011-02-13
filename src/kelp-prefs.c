0fb986640f25a4c6b0b7fbd1239072c68aa7e94a
Russell Sim <russell.sim@gmail.com>
2011-02-13 15:11:16 +1100

    added saving of port selection

--- src/kelp-prefs.c
+++ src/kelp-prefs.c
@@ -200,8 +200,21 @@ on_prefs_comptype_changed (GtkComboBox *combobox, Kelp *kelp)
 			gtk_tree_model_get (model, &iter,
 								COL_COMPUTER_T, &computer_type,
 								-1);
-			if (!g_key_file_has_group(file, "Computer"))
 			g_key_file_set_string (file, "Computer", "Type", computer_type);
 			save_kelp_preferences_file (file);
 		}
+}
+
+
+void
+on_prefs_port_file_set (GtkFileChooserButton *button, Kelp *kelp)
+{
+	GFile *portname = NULL;
+	GKeyFile *file;
+
+	portname = gtk_file_chooser_get_file (GTK_FILE_CHOOSER(button));
+	file = get_kelp_preferences_file();
+	g_key_file_set_string (file, "Computer", "Port", g_file_get_path (portname));
+	save_kelp_preferences_file (file);
+	g_object_unref(portname);
 }
