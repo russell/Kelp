cfdaac62365a703efef9718137023fcef1ee9c9f
Russell Sim <russell.sim@gmail.com>
2011-02-13 16:05:39 +1100

    fixed up copyright statements

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * main.c
- * Copyright (C) Russell Sim 2011 <russell.sim@gmail.com>
+ * Copyright (C) 2011 Russell Sim <russell.sim@gmail.com>
  *
  * kelp is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
